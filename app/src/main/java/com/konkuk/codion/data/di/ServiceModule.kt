9e14d8f6b3d5c335ebc92553231d821e56344777
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-06-09 22:10:08 +0900

    feat: 코디 기록 및 코멘트 API 서비스 인터페이스 추가

--- app/src/main/java/com/konkuk/codion/data/di/ServiceModule.kt
+++ app/src/main/java/com/konkuk/codion/data/di/ServiceModule.kt
@@ -2,6 +2,8 @@ package com.konkuk.codion.data.di
 
 import com.konkuk.codion.data.service.AuthService
 import com.konkuk.codion.data.service.ClosetService
+import com.konkuk.codion.data.service.CoordinationService
+import com.konkuk.codion.data.service.MyPageService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,4 +23,14 @@ object ServiceModule {
     @Singleton
     fun providesClosetService(retrofit: Retrofit): ClosetService =
         retrofit.create(ClosetService::class.java)
+
+    @Provides
+    @Singleton
+    fun providesMyPageService(retrofit: Retrofit): MyPageService =
+        retrofit.create(MyPageService::class.java)
+
+    @Provides
+    @Singleton
+    fun providesCoordinationService(retrofit: Retrofit): CoordinationService =
+        retrofit.create(CoordinationService::class.java)
 }
